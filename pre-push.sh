d162ce44f684e5b54c91c95f37e4ea3089dec51e
root <root@ip-10-0-27-87.ap-south-1.compute.internal>
2023-10-06 06:11:37 +0000

    new

--- pre-push.sh
+++ pre-push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Run Talisman scan
-talisman --scan
+# Run Talisman scan and capture the report in a variable
+talisman_report=$(talisman --scan)
 
 # Check if Talisman reported any issues
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
     # Save the report to a temporary file
-    talisman --scan > talisman_report.txt
+    echo "$talisman_report" > talisman_report.txt
 
     # Upload the report to an S3 bucket
-    aws s3 cp talisman_report/talisman_reports/data/report.json s3://zues2023/
+    aws s3 cp talisman_report.txt s3://zues2023/
 fi
+
